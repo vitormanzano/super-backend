bccd5bd0c29b9972b2d8e61f0c86338a25269408
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-09-18 11:27:43 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,46 @@
     usando o http sem express.
 */
 import http from "http";
+const PI:number = 3.1415;
+const port: number = 3000;
 
-const port = 3000;
+function CalcCompCircunferencia(r:number ): number {
 
-const server = http.createServer((request, response)=>{
-    // vou criar as rotas do meu servidor aqui.
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    response.end('<h1>It works!</h1>');
+    return 2*PI*r;
+
+}
+
+function calcAreaCirculo(r:number):number {
+    return PI*r*r;
+}
+
+const server = http.createServer((req, res)=>{
+
+    if(req.url  ===  "/CalcAreaCirculo"){
+        const area:number = calcAreaCirculo(10);
+        res.writeHead(200,{'Content-type': 'text-plain'});
+        res.end(area.toString());
+
+    }
+    else if (req.url === "/CalcCompCircunferencia"){
+        const comp:number = CalcCompCircunferencia(10)
+        res.writeHead(200,{'Content-type': 'text-plain'});
+        res.end(comp.toString());
+
+    }
+    else {
+        res.writeHead(404,{'Content-type': 'text-plain'});
+        res.end('Servico inexistente');
+
+    }
 });
 
 server.listen(port,()=>{
     console.log(`Servidor rodando na porta ${port}`);
 });
+
+
+
+
+
+
