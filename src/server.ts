94a92679e3323ba575aa8a02636ce3193e8cd84f
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2024-09-24 13:56:44 -0300

    tudo

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
     Servidor simples
     usando o http sem express.
 */
-import http from "http";
+import http, { request } from "http";
 const PI:number = 3.1415;
 const port: number = 3000;
 
@@ -16,15 +16,21 @@ function calcAreaCirculo(r:number):number {
     return PI*r*r;
 }
 
+
+
+
+
 const server = http.createServer((req, res)=>{
 
     if(req.url  ===  "/CalcAreaCirculo"){
+        
         const area:number = calcAreaCirculo(10);
         res.writeHead(200,{'Content-type': 'text-plain'});
         res.end(area.toString());
 
     }
     else if (req.url === "/CalcCompCircunferencia"){
+ 
         const comp:number = CalcCompCircunferencia(10)
         res.writeHead(200,{'Content-type': 'text-plain'});
         res.end(comp.toString());
